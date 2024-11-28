9c62a343be97867cf202e979ab9a208de9ce99d8
Daniele Souza <danielesouzadev@gmail.com>
2024-11-28 11:07:58 -0300

    commit inicial

---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,35 @@
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+function TaskPage() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button
+            onClick={() => navigate(-1)} //para o botão voltar p/ a pagina inicial
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-slate-100 text-3xl font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskPage;
+
+// Botão de voltar esse <button>
+// Sempre que estiver uma função com use é que esta utilizando o hook.
